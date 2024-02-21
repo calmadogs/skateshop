794db66164ecd42f78c041961802be46e30a91cd
Yago <yago-oliveira1012hotmail.com>
2024-02-20 22:07:19 -0300

    Feature - registrando classe no admin.py. Feature adicionando id e classe Meta no models.py. Feature - Adicionando classe na views.py

--- polls_skate_parts/models.py
+++ polls_skate_parts/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class SkateParts(models.Model):
+    id = models.BigIntegerField(primary_key=True)
     shape_size = models.FloatField()
-    truck_size = models.FloatField()
+    truck_size = models.IntegerField()
     wheels_size = models.IntegerField()
     bearing_brand = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "skate_parts"
+        ordering = ["id"]
--- polls_skate_parts/views.py
+++ polls_skate_parts/views.py
@@ -1,7 +1,18 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import SkateParts
 
 
 class SkatePartsListView(ListView):
+    """
+    ListView: To display a list of objects.
+    """
     model = SkateParts
     context_object_name = 'my_parts'
+
+
+class SkatePartsDetailView(DetailView):
+    """
+    DetailView: To display details of a single object.
+    """
+    model = SkateParts
+    context_object_name = 'my_part'
